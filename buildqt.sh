ea757757dac4601298b4bbd09fcfbc6f21bf87cc
Jarko Vihriala <jarko.vihriala@jolla.com>
2014-02-07 21:23:27 +0200

    [files] add scripts to build qt and qtc.

---
+++ buildqt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$1" == "" ];then
+  echo : "$0 <builddir> <clean>"
+  exit 1
+fi
+if [ "$2" == "clean" ];then
+  if [ "$2" == "$PWD" ];then
+     echo "no, not your homedir."
+     exit 1
+  fi
+  rm -rf $1
+fi
+
+mkdir -p $1
+cd $1
+~/QtSrc/qt/configure -release -no-scripttools -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -opensource -confirm-license -nomake examples -nomake demos -prefix $PWD -DENABLE_VIDEO=0 -gtkstyle
+
+make -j$(cat /proc/cpuinfo | grep processor | wc -l)
+make install
---
+++ buildqtc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$1" == "" ];then
+  echo "usage: $0 <qtdir> <variantid> <full>"
+  exit 1
+fi
+
+VARIANT=$2
+
+if [ "$2" == "" ];then
+  VARIANT=SailfishAlpha4
+fi
+
+export INSTALL_ROOT=~/QtCreatorInstall
+export QTDIR=$1
+export QMAKESPEC=linux-g++-64
+export QT_PRIVATE_HEADERS=$QTDIR/include
+export PATH=$QTDIR/bin:$PATH
+
+$QTDIR/bin/qmake ~/QtCreatorSrc/digia-qt-creator/qtcreator.pro -r -after "DEFINES+=REVISION=jolla IDE_COPY_SETTINGS_FROM_VARIANT=. IDE_SETTINGSVARIANT=$VARIANT" QTC_PREFIX=
+
+if [ "$3" == "full" ];then
+  make -j$(cat /proc/cpuinfo | grep processor | wc -l)
+  make install
+  make deployqt
+  make bindist_installer
+fi
