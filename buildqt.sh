506a4b4a3692770660fd1bfdc51c034764c30c20
Jarko Vihriala <jarko.vihriala@jolla.com>
2014-03-05 12:57:30 +0200

    [static] rework qt4 building to work correctly with static build.

--- buildqt.sh
+++ buildqt.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-if [ "$2" == "static" ]; then
-	STATIC="-static"
+QTSRC=$1
+
+function configure_static_qt4 () {
+	$QTSRC/configure -opensource -release -static -accessibility -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -no-phonon -no-dbus -no-opengl -no-qt3support -no-webkit -no-xmlpatterns -no-svg -nomake examples -nomake demos -prefix $PWD -confirm-license
+}
+
+function configure_qt4 () {
+	$QTSRC/configure -release -no-scripttools -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -opensource -confirm-license -nomake examples -nomake demos -prefix $PWD -DENABLE_VIDEO=0 -gtkstyle
+}
+
+if [ "$1" == "" ];then
+	echo "usage: $0 <QTSRC> <static>"
+	exit 1;
 fi
 
-$1/configure $STATIC -release -no-scripttools -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -opensource -confirm-license -nomake examples -nomake demos -prefix $PWD -DENABLE_VIDEO=0 -gtkstyle
+if [ "$2" == "static" ];then
+	configure_static_qt4
+else
+	configure_qt4
+fi
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
+
