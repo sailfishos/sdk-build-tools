8e8d234f62d0e3aa1517c6a65f7334f981b84580
Jarko Vihriala <jarko.vihriala@jolla.com>
2014-03-05 23:08:05 +0200

    [static] rework the static qt4 build.

--- buildqt.sh
+++ buildqt.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+set -x
+
 QTSRC=$1
 
+CWD=$PWD
+
+if [ -f $QTSRC/Makefile ]; then
+	cd $QTSRC
+	git clean -xdf
+	cd $CWD
+fi
+
+
 function configure_static_qt4 () {
-	$QTSRC/configure -opensource -release -static -accessibility -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -no-phonon -no-dbus -no-opengl -no-qt3support -no-webkit -no-xmlpatterns -no-svg -nomake examples -nomake demos -prefix $PWD -confirm-license
+	cd $QTSRC
+	./configure -opensource -release -static -accessibility -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -no-phonon -no-dbus -no-opengl -no-qt3support -no-webkit -no-xmlpatterns -no-svg -nomake examples -nomake demos -prefix $1 -confirm-license
 }
 
 function configure_qt4 () {
@@ -16,7 +28,7 @@ if [ "$1" == "" ];then
 fi
 
 if [ "$2" == "static" ];then
-	configure_static_qt4
+	configure_static_qt4 $3
 else
 	configure_qt4
 fi
