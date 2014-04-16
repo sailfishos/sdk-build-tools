6cddf5ffa7f67ed729af027f1517e2c4a3bc5ade
Juha Kallioinen <juha.kallioinen@jolla.com>
2014-04-16 23:49:39 +0300

    [info] added file info script
    Signed-off-by: Juha Kallioinen <juha.kallioinen@jolla.com>

---
+++ file-information.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+#
+# Create file information page for SDK release
+#
+# Copyright (C) 2014 Jolla Oy
+# Contact: Juha Kallioinen <juha.kallioinen@jolla.com>
+# All rights reserved.
+#
+# You may use this file under the terms of BSD license as follows:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the Jolla Ltd nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+RELEASE="1406"
+RELEASE_CYCLE="Alpha"
+CHECK="md5"
+PLATFORMS="linux-32 linux-64 mac windows"
+
+fatal() {
+    echo "FAIL: $@"
+    exit 1
+}
+
+usage() {
+    cat <<EOF
+
+Create output that can be used in SDK release file information page.
+
+This will calculate the md5sum for each file and it can take a
+while. Be patient.
+
+Usage:
+   $(basename $0) [OPTION]
+
+Options:
+   -r  | --release [LABEL]    release label [$RELEASE]
+   -h  | --help               this help
+
+EOF
+
+    # exit if any argument is given
+    [[ -n "$1" ]] && exit 1
+}
+
+
+# handle commandline options
+while [[ ${1:-} ]]; do
+    case "$1" in
+	-r | --release) shift
+	    OPT_RELEASE=$1; shift
+	    [[ -z $OPT_RELEASE ]] && fatal "release label required"
+	    RELEASE=$OPT_RELEASE
+	    ;;
+        -h | --help ) shift
+            usage quit
+            ;;
+        * )
+            usage quit
+            ;;
+    esac
+done
+
+
+echo "See also [https://sailfishos.org/wiki/ReleaseNote_SDK_$RELEASE '''Release notes for SDK release $RELEASE''']."
+
+echo "==File information=="
+
+for ARCH in $PLATFORMS; do
+    if [[ $ARCH == "mac" ]]; then
+	SUFFIX="dmg"
+    elif [[ $ARCH == "windows" ]]; then
+	SUFFIX="exe"
+    else
+	SUFFIX="run"
+    fi
+
+    FNAME=SailfishOSSDK-$RELEASE_CYCLE-$RELEASE-Qt5-$ARCH-offline.$SUFFIX
+    [[ ! -f $FNAME ]] && fatal "$FNAME not found."
+    md5sum -b $FNAME > $FNAME.$CHECK
+    FSIZE=$(stat -c %s $FNAME)
+    FSIZE_MIB=$(ls -lh $FNAME | cut -f 5 -d ' ')
+    MD5=$(cut -f 1 -d ' ' $FNAME.$CHECK)
+    
+    cat <<EOF
+* [http://releases.sailfishos.org/sdk/installers/$RELEASE/$FNAME Filename]: $FNAME
+* Size: $FSIZE_MIB ($FSIZE bytes)
+* [http://releases.sailfishos.org/sdk/installers/$RELEASE/$FNAME.$CHECK MD5 Hash]: $MD5
+
+EOF
+
+done
