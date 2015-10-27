fae56387371349fc202f57642dc1224f93a2bbc8
Martin Kampas <martin.kampas@jolla.com>
2015-10-27 20:04:45 +0100

    buildqt5.sh: Configure windows static build with -static-runtime

--- buildqt5.sh
+++ buildqt5.sh
@@ -143,7 +143,7 @@ if Not DEFINED ProgramFiles(x86) set _programs=%ProgramFiles%
 
 set PATH=c:\windows;c:\windows\system32;%_programs\windows kits\8.0\windows performance toolkit;%_programs%\7-zip;C:\invariant\bin;c:\python27;c:\perl\bin;c:\ruby193\bin;c:\invariant\icu\bin;C:\invariant\\$QT_SOURCE_PACKAGE\gnuwin32\bin;%_programs%\microsoft sdks\typescript\1.0;c:\windows\system32\wbem;c:\windows\system32\windowspowershell\v1.0;c:\invariant\bin
 call "%_programs%\microsoft visual studio 12.0\vc\vcvarsall.bat"
-call c:\invariant\\$QT_SOURCE_PACKAGE\configure.bat $COMMON_CONFIG_OPTIONS $COMMON_STATIC_OPTIONS -angle -platform win32-msvc2012 -prefix
+call c:\invariant\\$QT_SOURCE_PACKAGE\configure.bat $COMMON_CONFIG_OPTIONS $COMMON_STATIC_OPTIONS -angle -platform win32-msvc2012 -static-runtime -prefix
 
 call jom /j 1
 EOF
