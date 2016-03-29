sailfishos/sdk-build-tools

commit ec6f3024988801e89e977e9928217ddd0046fdc6

buildmaster.sh: Use git-hard-reset instead of pull
This is to survive non fast-forward upstream updates.

buildmaster.sh
@@ -365,7 +365,8 @@ do_git_pull() {
         _ git clean -xdf
         _ git reset --hard
         _ git checkout ${REQUIRED_GIT_BRANCHES[i]}
-        _ git pull
+        upstream=$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD))
+        _ git reset --hard $upstream
         _ popd
     done
 }

commit 998ef108db4aeb11c845e6d41c601ce1e266008e

Fix pulling sources
Broken since ec6f302 (buildmaster.sh: Use git-hard-reset instead of pull)

buildmaster.sh
@@ -337,6 +337,7 @@ do_git_pull() {
         _ git clean -xdf
         _ git reset --hard
         _ git checkout ${REQUIRED_GIT_BRANCHES[i]}
+        _ git fetch --all
         upstream=$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD))
         _ git reset --hard $upstream
         _ popd