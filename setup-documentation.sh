596cc67fbbec7fd5ae9314bdffb3a295ffcb4b38
Ville Nummela <ville.nummela@jolla.com>
2019-09-19 10:52:38 +0300

    setup-documentation.sh: add libkeepalive documentation

--- setup-documentation.sh
+++ setup-documentation.sh
@@ -207,7 +207,7 @@ done
 rm -f $INSTALL_PATH/*
 
 # create Sailfish documentation package
-for NAME in $(ls $OPT_DOCDIR/{sdk-,*sailfish,lipstick,nemo}*.qch); do
+for NAME in $(ls $OPT_DOCDIR/{sdk-,*sailfish,lipstick,nemo,libkeepalive}*.qch); do
     if ! add_filter "$NAME" "$DEF_DOCS_FILTER_ATTRIBUTE" "$DEF_DOCS_FILTER_NAME"; then
         failure "Failed to add filter to '$NAME'"
     fi
